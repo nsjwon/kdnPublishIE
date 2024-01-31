abd696054974abcbc2227d5812c8d5a5840ff018
nsjwon <nsjaewon@gmail.com>
2024-01-31 15:24:30 +0900

    240131_메인페이지 1280해상도 적용

--- js/common.js
+++ js/common.js
@@ -1,4 +1,28 @@
-//사이드 메뉴 클릭이벤트
+//헤더 메뉴 드롭다운
+$(document).ready(function() {
+    $("#gnb").mouseenter(function() {
+        // 마우스가 올라갔을 때
+        $("#header").addClass("on");
+        $(".gnb-bg").addClass("on");
+        $(".depth2").addClass("on");
+        $(".dim").show();
+    }).mouseleave(function() {
+        // 마우스가 나갔을 때
+        $("#header").removeClass("on");
+        $(".gnb-bg").removeClass("on");
+        $(".depth2").removeClass("on");
+        $(".dim").hide();
+    });
+
+    // $(document).ready(function() {
+    //     $("#gnb>ul>li").hover(function() {
+    //         $("#gnb>ul>li").removeClass("focus");
+    //         $(this).addClass("focus");
+    //     });
+    // });
+});   
+
+//레프트 메뉴 클릭이벤트
 $(document).ready(function() {
     $(".d1 > a").click(function(e) {
         e.preventDefault(); // 기본 동작을 막음
@@ -16,41 +40,32 @@ $(document).ready(function() {
         // 현재 클릭한 one-depth 메뉴에 active 클래스를 추가
         parentD1.toggleClass("active");
     });
+});
 
-    $(document).ready(function() {
-        $("#gnb").mouseenter(function() {
-            // 마우스가 올라갔을 때
-            $("#header").addClass("on");
-            $(".gnb-bg").addClass("on");
-            $(".depth2").addClass("on");
-            $(".dim").show();
-        }).mouseleave(function() {
-            // 마우스가 나갔을 때
-            $("#header").removeClass("on");
-            $(".gnb-bg").removeClass("on");
-            $(".depth2").removeClass("on");
-            $(".dim").hide();
-        });
+//메인페이지 제안검토 카테고리 설정 셀렉트박스
+$(document).ready(function() {
+    $(".ct-select > a").click(function(event) {
+        $(".ct-select").toggleClass("open");
+        if ($(".ct-select").hasClass("open")) {
+            $(".ct-option").show();
+        } else {
+            $(".ct-option").hide();
+        }
+        event.stopPropagation();
     });
-    
-    
-    $(document).ready(function() {
-        $(".ct-select > a").click(function() {
-            $(".ct-select").toggleClass("open");
 
-            if ($(".ct-select").hasClass("open")) {
-                $(".ct-option").show();
-            } else {
-                $(".ct-option").hide();
-            }
-
-        });
-
-        $(".ct-option li a").click(function() {
-            var selectedOptionText = $(this).text();
-            $(".ct-select > a").text(selectedOptionText);
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.ct-select').length) {
             $(".ct-option").hide();
             $(".ct-select").removeClass("open");
-        });
+        }
+    });
+
+    $(".ct-option li a").click(function() {
+        var selectedOptionText = $(this).text();
+        $(".ct-select > a").text(selectedOptionText);
+        $(".ct-option").hide();
+        $(".ct-select").removeClass("open");
     });
 });
+
